c5c711462f0f0ca6a4b27438456c5f223c343733
AvihayR <avihayrabi@gmail.com>
2023-09-06 12:03:05 +0300

    Init project

---
+++ apps/mail/services/demo-data.js
@@ -0,0 +1,25 @@
+
+export const demoData = [
+    {
+        id: 'e101',
+        subject: 'Miss you!',
+        body: 'Would love to catch up sometimes',
+        isRead: false,
+        sentAt: 1551133930594,
+        removedAt: null,
+        from: 'momo@momo.com',
+        to: 'user@appsus.com'
+    },
+    {
+        id: 'e102',
+        subject: 'קבלת רכישה ב-Wolt',
+        body: 'א, איזה כיף שהזמנת',
+        isRead: false,
+        sentAt: 1551133930594,
+        removedAt: null,
+        from: 'Wolt',
+        to: 'user@appsus.com'
+    },
+
+]
+
