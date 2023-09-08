07f4e1c872581a8c4a1a0f8f92d148b16c74c433
Raz <amsalemraz10@gmail.com>
2023-09-08 15:52:34 +0300

    Home page designed

--- views/Home.jsx
+++ views/Home.jsx
@@ -1,6 +1,49 @@
+const { useState, useEffect } = React
+const { Link } = ReactRouterDOM
 
 export function Home() {
-    return <section className="home">
-        <h1>Welcome to home page!</h1>
-    </section>
+
+    const colors = [
+        '#d62d20',
+        '#0b57d0',
+        '#008744',
+        '#ffa700',
+        '#0b57d0'
+    ]
+
+    const sentences = [
+        'All your needs in one place',
+        'Capture ideas, make to-do lists, and set reminders effortlessly',
+        'Stay organized',
+        'Your trusted email platform',
+        'Access your email from anywhere'
+    ]
+    const [currentSentence, setCurrentSentence] = useState(0)
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setCurrentSentence((prevSentence) => (prevSentence + 1) % sentences.length)
+        }, 3000)
+
+        return () => clearTimeout(timer);
+    }, [currentSentence, sentences]);
+
+    return (
+        <section className="home">
+            <div className="center">
+                <div className="logo-container">
+                    <img src="../assets/img/logo.png" alt="Logo" className="logo" />
+                </div>
+                <div className="text-container">
+                    <div className="typing-animation" style={{ color: colors[currentSentence] }}>
+                        {sentences[currentSentence]}
+                    </div>
+                </div>
+                <div className="home-buttons-container">
+                    <Link to="/mail" className="gmail-button">Mail</Link>
+                    <Link to="/note" className="keep-button">Notes</Link>
+                </div>
+            </div>
+        </section>
+    );
 }
